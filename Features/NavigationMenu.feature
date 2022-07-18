0fa3c9b0e4f2ad1db3831947326b6066ba681547
ajay.adenusi <ajay.adenusi@ASAZ-GRR13M3>
2022-07-18 16:44:18 +0100

    Add project files.

---
+++ Features/NavigationMenu.feature
@@ -0,0 +1,13 @@
+﻿Feature: Verify all the top navigation menu links are clickable and display content
+
+@tag1
+Scenario Outline: Verify the navigation menu Limited time only displays some content
+	
+	Given I am on myunidays Homepage 
+	When I click on a navigation <menu>
+	Then a content is displayed
+
+Examples:
+
+	| menu               |
+	| Limited time only! |
---
+++ StepDefinitions/NavigationMenu.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using TechTalk.SpecFlow;
+
+[Binding]
+public class NavigationMenu
+{
+    String base_url = "https://www.myunidays.com/";
+  private IWebDriver driver;
+
+
+    //public NavigationMenu(ScenarioContext injectedContext)
+    //{
+    //    context = injectedContext;
+    //}
+  
+
+    [Given(@"I am on myunidays Homepage")]
+    [Test]
+    public void GivenIAmOnMyunidaysHomepage()
+    {
+        driver = new FirefoxDriver();
+        driver.Url = base_url;
+        driver.Manage().Window.Maximize();
+        System.Threading.Thread.Sleep(2000);
+    }
+
+    [When(@"I click on a navigation (.*)")]
+    public void WhenIClickOnANavigationMenu(string menu)
+    {
+        IWebElement textfield = driver.FindElement(By.XPath("Limited time only!"));
+       textfield.Click();
+    }
+
+    [Then(@"a content is displayed")]
+    public void ThenAContentIsDisplayed()
+    {
+        throw new PendingStepException();
+    }
+
+
+
+}
